786f38930431507cc3eebbfba2c624a3223ae705
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-07-01 12:58:57 +0200

    changes

--- src/main/java/com/CrudTutorial/controllers/TutorialControllers.java
+++ src/main/java/com/CrudTutorial/controllers/TutorialControllers.java
@@ -14,6 +14,7 @@ public class TutorialControllers {
 
     @Autowired
     TutorialService tutorialService;
+
     @DeleteMapping(path = "/tutorials")
     public void deleteAllTutorial(){
         tutorialService.deleteAllTutorial();
