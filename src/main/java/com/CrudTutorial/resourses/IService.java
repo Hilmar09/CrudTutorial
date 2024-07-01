3655d424a50bb99223337523521b246cc963ec3b
Ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-07-01 09:08:29 +0200

    fixes

--- src/main/java/com/CrudTutorial/resourses/IService.java
+++ src/main/java/com/CrudTutorial/resourses/IService.java
@@ -2,7 +2,6 @@ package com.CrudTutorial.resourses;
 
 import com.CrudTutorial.models.Tutorial;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.scheduling.config.Task;
 
-public interface IServise extends CrudRepository<Tutorial, Integer> {
+public interface IService extends CrudRepository<Tutorial, Integer> {
 }
