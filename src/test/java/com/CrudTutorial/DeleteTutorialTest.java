3b97cd208d454eec2ab6859c5e438b5ac5d242dd
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-07-02 09:10:46 +0200

    pull changes

--- src/test/java/com/CrudTutorial/DeleteTutorialTest.java
+++ src/test/java/com/CrudTutorial/DeleteTutorialTest.java
@@ -1,13 +1,13 @@
 package com.CrudTutorial;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Assertions.assertEquals;
-@SpringBootTest
-class CrudTutorialApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+@SpringBootTest
+public class DeleteTutorialTest {
+ @Test
+    void deleteAllTutorial(){
+
+ }
 
 }
